7393591e88f5ae857824db3b597f60d32e24d0d5
Sindunata Sudarmaji <sindux@gmail.com>
2014-07-15 23:55:02 +0900

    Clean up

--- src/main/scala/code/rss/infoq.scala
+++ src/main/scala/code/rss/infoq.scala
@@ -18,19 +18,21 @@ import net.liftweb.http.rest.RestHelper
 import net.liftweb.common.Logger
 import scala.xml.Attribute
 import net.liftweb.http.rest.RestContinuation
+import scala.util.matching.Regex
+import com.ning.http.client.Response
 
 object infoq extends RestHelper with Logger {
   serve { 
     case "rss" :: "video" :: Nil Get req =>
-      val req = httpRequest("http://www.infoq.com/feed/presentations")
+      val req = httpRequest(as.xml.Elem)(Map.empty)("http://www.infoq.com/feed/presentations")
       val newRssFut = for {
           response <- req
           itemUrls = getItemUrls(response)
-          itemPagesFut = itemUrls map httpRequest2
+          itemPagesFut = itemUrls map httpRequest(as.String)(ipadUA)
           itemPagesFutAll = Future.sequence(itemPagesFut)
           itemPages <- itemPagesFutAll
-          videoUrls = itemPages map getVideoUrl
-          thumbnailUrls = itemPages map getThumbnail
+          videoUrls = itemPages map parseUrl(videoRegex)
+          thumbnailUrls = itemPages map parseUrl(thumbnailRegex) map addPrefix("http://www.infoq.com")
           enclosureRule = new AddEnclosure(
 	          (itemUrls zip videoUrls).toMap,
 	          (itemUrls zip thumbnailUrls).toMap)
@@ -40,14 +42,10 @@ object infoq extends RestHelper with Logger {
       RestContinuation.async(reply => for (newRss <- newRssFut) yield reply(newRss))
   }
 
-  def httpRequest(uri: String): Future[Elem] = {
-    val svc = url(uri)
-    Http(svc OK as.xml.Elem)
-  }
-  
-  def httpRequest2(uri: String): Future[String] = {
-    val svc = url(uri) <:< Map("User-Agent" -> "Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10")
-    Http(svc OK as.String)
+  val ipadUA = Map("User-Agent" -> "Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10")
+  def httpRequest[T](retType: Response => T)(headers: Map[String, String])(uri: String): Future[T] = {
+    val svc = url(uri) <:< headers
+    Http(svc OK retType)
   }
     
   class AddEnclosure(videoUrls: Map[String, Option[String]], thumbNailUrls: Map[String, Option[String]]) extends RewriteRule {
@@ -56,17 +54,11 @@ object infoq extends RestHelper with Logger {
 	    case <item>{children @ _*}</item> =>
 	        val linkNode = children collect {case <link>{link}</link> => {link}}
 	        val link = linkNode.head.text
-	        val videoUrl = videoUrls get link
-	        val thumbnailUrl = thumbNailUrls get link
-	        val vidTag = videoUrl match {
-	          case Some(Some(url)) => <enclosure url={url} type="video/mp4"/>
-	          case _ => Nil
-	        }
-	        val thumbnailTag = thumbnailUrl match {
-	          case Some(Some(url)) => <media:thumbnail url={url}/>
-	          case _ => Nil
-	        }
-	        <item>{children ++ vidTag}</item>
+	        val videoUrl = videoUrls.getOrElse(link, None)
+	        val thumbnailUrl = thumbNailUrls.getOrElse(link, None)
+	        val vidTag = for (url <- videoUrl) yield <enclosure url={url} type="video/mp4"/>
+	        val thumbnailTag = for (url <- thumbnailUrl) yield <media:thumbnail url={url}/> // why I need this comment, otherwise scala infers as Option[NodeBuffer]
+	        <item>{children ++ vidTag.getOrElse(Nil) ++ thumbnailTag.getOrElse(Nil)}</item>
 	    case other => other
 	  }
 	}
@@ -75,22 +67,16 @@ object infoq extends RestHelper with Logger {
   def getItemUrls(rssFeed: Elem): Seq[String] = {
     rssFeed \ "channel" \ "item" \ "link" map (_.text)
   }
-  
-  def getVideoUrl(page: String): Option[String] = {
-    val vidRegex = """(?s)<video poster="(.*?)".*?<source src="(.*?)"""".r
-    val matches = vidRegex findFirstMatchIn page
-    matches match {
-      case Some(url) => Some(url.group(2))
-      case _ => None
-    }
+ 
+  val videoRegex = """(?s)<video poster=".*?".*?<source src="(.*?)"""".r
+  val thumbnailRegex = """(?s)var slides = new Array\('(.*?)'""".r
+
+  def addPrefix(prefix: String)(str: Option[String]): Option[String] = {
+    for (s <- str) yield prefix + s
   }
   
-  def getThumbnail(page: String): Option[String] = {
-    val vidRegex = """(?s)var slides = new Array\('(.*?)'""".r
-    val matches = vidRegex findFirstMatchIn page
-    matches match {
-      case Some(url) => Some("http://www.infoq.com" + url.group(1))
-      case _ => None
-    }
+  def parseUrl(regex: Regex)(page: String): Option[String] = {
+    val matches = regex findFirstMatchIn page
+    for (m <- matches) yield m.group(1)
   }
 }
