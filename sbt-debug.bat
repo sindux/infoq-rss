7803067a4276d28817533c2ae549ac784d0c811b
Sindunata Sudarmaji <sindux@gmail.com>
2014-10-04 00:16:28 +0900

    Start clean

--- sbt-debug.bat
+++
@@ -1,2 +0,0 @@
-set SCRIPT_DIR=%~dp0
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx1024M -Xss2M -jar "%SCRIPT_DIR%\sbt-launch-0.13.1.jar" %*
